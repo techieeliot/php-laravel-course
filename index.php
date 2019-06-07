eb2eaeec7d22e3db43342d698d6de703c3227a59
Eliot Sanford <eliot587@gmail.com>
2019-06-06 22:19:38 -0500

    created html and some css

--- index.php
+++ index.php
@@ -1,14 +1,71 @@
+<!-- 
+  1. What factors impact the score of a website:
+      access page-speed, 
+      headless chrome, 
+      malware,
+      email blacklists
+      ???
+    
+
+  2. score the site based on the factors
+
+ -->
+
+ <?php 
+ if(!empty($_GET['url']){}
+ ?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <link rel="stylesheet" href="/assets/styles.css">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Eliot's PHP App</title>
+  <title>Scorecard - Website Scoring</title>
 </head>
 <body>
-  <h1>Eliot's PHP App</h1>
-  <h2>A Codingphase Course</h2>
+  <main>
+    <header>
+      <h1>Scorecard Application</h1>
+      
+    </header>  
+
+    <section class="container">
+      <article class="description left">
+        <h2>The fancy widget scorecard</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus cupiditate, laboriosam blanditiis voluptatibus mollitia at autem similique placeat voluptatem earum aliquid nihil et corporis obcaecati quaerat soluta accusantium optio totam.</p>
+
+
+      </article>
+
+      <div class="center form">
+        <h2>Scorecard App Ranking Check</h2>
+        <form action="">
+          <label for="url">Enter your website here</label>
+          <input type="text" name="url" placeholder="Enter a URL Here">
+          <button type="submit">Show My Score!</button>
+        </form>
+      </div>
+
+      <aside class="right output">
+
+      </aside>
+
+    </section>
+    
+
+    <footer>
+      <header>Thanks for stopping by</header>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore provident nam numquam adipisci non iure itaque modi! Ab placeat, provident, itaque quia molestias numquam quas minima magni facere atque tempora?</p>
+    </footer>
+
+  
+  </main>
+
+
   
 </body>
 </html>
